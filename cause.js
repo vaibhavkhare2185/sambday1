3ce407bf793d2d260b8aaf0c3d3c24b48d042b33
Vaibhav Khare <vaibhavkhare2185@gmail.com>
2026-05-07 23:00:11 +0530

    Update cause.js

--- cause.js
+++ cause.js
@@ -1,27 +1,22 @@
  // Reasons database
  const reasons = [
     { 
-        text: "Youuuuuu areeeeee my favorite..."
-"My favorite pair of eyes to look into"
-"My favorite name to appear on my phone"
-"In a world so loud, you feel like home"
-"The way your smile lingers after a joke,"
-"The comfort I feel in every word you spoke. You became my favourite way to spend a afternoon", 
+        text: "With the threads of my veins, I would stitch you a blanket to keep the cold away." 
         emoji: "🌟",
         gif: "gif1.gif"
     },
     { 
-        text: "May your day be filled with love, laughter, and endless joy. 🌸 ", 
+        text: "I would lay down my bones like steping stones, just you wouldn't have to walk alone.", 
         emoji: "💗",
         gif: "gif2.gif"
     },
     { 
-        text: "Wishing you success, happiness, and everything your heart desires. ✨ ", 
+        text: "Without you I am just a devastated town with wars may you enter and rescue me.", 
         emoji: "💕",
         gif: "gif1.gif"
     },
     { 
-        text: "Stay the amazing girl you are—always spreading positivity around. Have the happiest year ahead! 🥳 ", 
+        text: "I love you so much, I love with every flesh of my heart, every drop of my blood, every tissue of my body.", 
         emoji: "🌟",
         gif: "gif2.gif"
     }
